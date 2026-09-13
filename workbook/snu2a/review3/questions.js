b9c5c554fa840c6defcc195e2065281cafede07f
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-13 22:08:12 +0800

    Add SNU 2A 7-9 evaluation questions

---
+++ workbook/snu2a/review3/questions.js
@@ -0,0 +1,168 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 김민수 씨한테 <span class="source-underline">전화해</span> 봤어요?<br>B: 아까 전화를 (      ) 봤는데 통화 중이었어요.',
+    options: ['받아', '전해', '끊어', '걸어'],
+    answer: 3
+  },
+  {
+    id: 2,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 여기에서 <span class="source-underline">가까운 곳</span>에 꽃집이 있어요?<br>B: 아니요, 이 (      )에는 꽃집이 없어요.',
+    options: ['근처', '거리', '종점', '광장'],
+    answer: 0
+  },
+  {
+    id: 3,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 여기에서 <span class="source-underline">오른쪽으로 돌아가요</span>?<br>B: 아니요, (      ) 돼요.',
+    options: ['직진해야', '올라가야', '건너가야', '좌회전해야'],
+    answer: 3
+  },
+  {
+    id: 4,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 샤오밍 씨는 스트레스를 <span class="source-underline">받으면</span> 어떻게 해요?<br>B: 저는 농구를 하면서 스트레스를 (      ).',
+    options: ['줘요', '써요', '풀어요', '버려요'],
+    answer: 2
+  },
+  {
+    id: 5,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 여기에서 지하철역까지 (      ) 가야 해요?\nB: 아니요, 5분 정도만 걸어가면 돼요.',
+    options: ['잠깐', '한참', '나중에', '천천히'],
+    answer: 1
+  },
+  {
+    id: 6,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 나나 씨, 이 단어 알아요?\nB: 뜻은 아는데 (      ) 발음하는지 잘 모르겠어요.',
+    options: ['무슨', '무엇을', '얼마나', '어떻게'],
+    answer: 3
+  },
+  {
+    id: 7,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 단어 외우는 것이 힘들지요?\nB: 네, 배운 단어가 잘 생각나지 않을 때 정말 (      ).',
+    options: ['기뻐요', '외로워요', '답답해요', '즐거워요'],
+    answer: 2
+  },
+  {
+    id: 8,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 어디에서 내려 드릴까요?\nB: 학교 정문 앞에서 (      ) 주세요.',
+    options: ['세워', '바꿔', '건너', '나가'],
+    answer: 0
+  },
+  {
+    id: 9,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 요리 수업에 대해서 (      ) 게 있어서 전화했는데요.\nB: 네, 말씀하세요.',
+    options: ['등록할', '접수할', '문의할', '가입할'],
+    answer: 2
+  },
+  {
+    id: 10,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 줄리앙 씨, 오늘 말하기 대회가 있지요? 기분이 어때요?\nB: 많은 사람들 앞에서 발표해야 하니까 너무 (      ).',
+    options: ['고장이 나요', '긴장이 돼요', '운이 없어요', '마음에 들어요'],
+    answer: 1
+  },
+  {
+    id: 11,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '내년에는 기숙사에 살 것 같아요.',
+      '오늘 아침 10시에는 회의한 중이었어요.',
+      '유진 씨가 어제 잠을 잘 못 잔 것 같아요.',
+      '어제 아침에 늦어서 기차를 놓쳐 버렸어요.'
+    ],
+    answer: 1
+  },
+  {
+    id: 12,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '감기 때문에 집에서 쉬세요.',
+      '저분 성함이 어떻게 되는지 아세요?',
+      '선생님을 만나려면 사무실로 가세요.',
+      '어제 학교에 오다가 친구를 만났어요.'
+    ],
+    answer: 0
+  },
+  {
+    id: 13,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 오늘 수업 끝나면 콘서트 보러 갈 거예요.\nB: 아, 그래요? (      ).',
+    options: ['재미있네요', '재미있는데요', '재미있겠어요', '재미있는 것 같아요'],
+    answer: 2
+  },
+  {
+    id: 14,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 마리코 씨 생일 선물로 요리책이 어때요?\nB: 좋은 생각이에요. 마리코 씨 취미가 요리니까 (      ).',
+    options: ['좋아하면 돼요', '좋아할 것 같아요', '좋아하는지 알아요', '좋아했으면 좋겠어요'],
+    answer: 1
+  },
+  {
+    id: 15,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 통장을 (      ) 뭐가 필요해요?\nB: 신분증하고 도장이 있어야 돼요.',
+    options: ['만들면', '만들고', '만들려면', '만들려고'],
+    answer: 2
+  },
+  {
+    id: 16,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 숙제 다 했어요?\nB: 아니요, (      ) 피곤해서 그냥 잤어요.',
+    options: ['숙제하다가', '숙제하지만', '숙제하려고', '숙제하거나'],
+    answer: 0
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 실례지만 하나병원이 어디에 있습니까?<br>B: 3번 출구로 나가면 돼요.<br>A: 3번 출구에서 ( <span class="blank-highlight">㉠</span> )?<br>B: 아니요, 가까워요. 5분쯤 가면 병원이 ( <span class="blank-highlight">㉡</span> ).',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['먼가요', '보나요', '가나요', '짧은가요'],
+    answer: 0
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 실례지만 하나병원이 어디에 있습니까?<br>B: 3번 출구로 나가면 돼요.<br>A: 3번 출구에서 ( <span class="blank-highlight">㉠</span> )?<br>B: 아니요, 가까워요. 5분쯤 가면 병원이 ( <span class="blank-highlight">㉡</span> ).',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞지 않은 것을 고르세요.',
+    options: ['있어요', '보여요', '나와요', '만나요'],
+    answer: 3
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '오늘 친구가 사는 춘천에 다녀왔습니다. 친구도 만나고 그 친구가 다니는 대학교도 구경하러 갔습니다. 서울에서 춘천까지 전철을 타고 갔는데 한 시간 삼십 분밖에 ( <span class="blank-highlight">㉠</span> ). 우리는 함께 점심을 먹고 이야기를 했습니다. 그동안 자주 연락을 못 해서 친구가 어떻게 ( <span class="blank-highlight">㉡</span> ) 잘 몰랐는데 요즘 공부 때문에 많이 힘든 것 같습니다. 앞으로 시간을 내서 친구를 자주 만나러 갈 생각입니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['없었습니다', '멀지 않습니다', '걸린 것 같습니다', '걸리지 않았습니다'],
+    answer: 3
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '오늘 친구가 사는 춘천에 다녀왔습니다. 친구도 만나고 그 친구가 다니는 대학교도 구경하러 갔습니다. 서울에서 춘천까지 전철을 타고 갔는데 한 시간 삼십 분밖에 ( <span class="blank-highlight">㉠</span> ). 우리는 함께 점심을 먹고 이야기를 했습니다. 그동안 자주 연락을 못 해서 친구가 어떻게 ( <span class="blank-highlight">㉡</span> ) 잘 몰랐는데 요즘 공부 때문에 많이 힘든 것 같습니다. 앞으로 시간을 내서 친구를 자주 만나러 갈 생각입니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞은 것을 고르세요.',
+    options: ['아는지', '지내는지', '이해하는지', '연락하는지'],
+    answer: 1
+  }
+];
