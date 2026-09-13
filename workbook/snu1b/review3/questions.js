3b94821eb62234cdcd524141a129b83ddcadae73
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-13 15:00:07 +0800

    Add files via upload

---
+++ workbook/snu1b/review3/questions.js
@@ -0,0 +1,165 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-3] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q01.png',
+    prompt: 'A: 경주까지 어떻게 갈 거예요?\nB: (      )를 타고 갈 거예요.',
+    options: ['배', '기차', '버스', '택시'],
+    answer: 1
+  },
+  {
+    id: 2,
+    group: '[1-3] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q02.png',
+    prompt: 'A: 어제 뭘 샀어요?\nB: (      )을/를 샀어요.',
+    options: ['바지', '셔츠', '치마', '코트'],
+    answer: 0
+  },
+  {
+    id: 3,
+    group: '[1-3] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q03.png',
+    prompt: 'A: 근처에 (      )이/가 있어요?\nB: 네, 있어요.',
+    options: ['공항', '신호등', '지하철역', '버스 정류장'],
+    answer: 2
+  },
+  {
+    id: 4,
+    groupHtml: true,
+    group: '[4-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 책이 <span class="opposite-highlight">두꺼워요</span>?<br>B: 아니요, (      ).',
+    options: ['쉬워요', '얇아요', '귀여워요', '무거워요'],
+    answer: 1
+  },
+  {
+    id: 5,
+    groupHtml: true,
+    group: '[4-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 동생도 키가 <span class="opposite-highlight">커요</span>?<br>B: 아니요, (      ).',
+    options: ['낮아요', '많아요', '작아요', '짧아요'],
+    answer: 2
+  },
+  {
+    id: 6,
+    groupHtml: true,
+    group: '[4-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 고속버스 터미널이 <span class="opposite-highlight">가깝지요</span>?<br>B: 아니요, (      ).',
+    options: ['높아요', '멀어요', '예뻐요', '편해요'],
+    answer: 1
+  },
+  {
+    id: 7,
+    groupHtml: true,
+    group: '[4-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 그 구두 <span class="opposite-highlight">편해요</span>?<br>B: 아니요, 좀 (      ).',
+    options: ['똑똑해요', '복잡해요', '불편해요', '중요해요'],
+    answer: 2
+  },
+  {
+    id: 8,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '날씨가 추워서 모자를 (      ).',
+    options: ['썼어요', '내렸어요', '신었어요', '입었어요'],
+    answer: 0
+  },
+  {
+    id: 9,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '(      ) 사람을 좋아하세요?',
+    options: ['누구', '무슨', '어떤', '어떻게'],
+    answer: 2
+  },
+  {
+    id: 10,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '히엔 씨는 숙제를 (      ) 안 했어요.',
+    options: ['꼭', '푹', '아직', '이따'],
+    answer: 2
+  },
+  {
+    id: 11,
+    group: '[11-13] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 어느 나라 사람이세요?\nB: 일본(      ) 왔어요.',
+    options: ['에', '까지', '부터', '에서'],
+    answer: 3
+  },
+  {
+    id: 12,
+    group: '[11-13] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 이 버스가 시청(      ) 가요?\nB: 아니요, 안 가요.',
+    options: ['에서', '으로', '하고', '한테'],
+    answer: 1
+  },
+  {
+    id: 13,
+    group: '[11-13] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 지금 뭐 해요?\nB: 아버지(      ) 편지 쓰고 있어요.',
+    options: ['께', '로', '에', '까지'],
+    answer: 0
+  },
+  {
+    id: 14,
+    group: '[14-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 내일 데이트에 뭘 입고 갈 거예요?\nB: 원피스를 (      ).',
+    options: ['입었어요', '입고 있어요', '입지 마세요', '입으려고 해요'],
+    answer: 3
+  },
+  {
+    id: 15,
+    group: '[14-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 주말에 책을 읽으려고 해요. 무슨 책을 읽을까요?\nB: 이 책이 재미있어요. 이 책을 한번 (      ).',
+    options: ['읽을까요', '읽어 보세요', '읽지 마세요', '읽으려고 해요'],
+    answer: 1
+  },
+  {
+    id: 16,
+    group: '[14-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 어떤 운동화를 찾으세요?\nB: 편하고 (      ) 운동화를 사려고 해요.',
+    options: ['가벼운', '가볍고', '가벼워서', '가볍지만'],
+    answer: 0
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 어서 오세요. 어디로 갈까요?<br>B: 서울역으로 가 주세요. 서울역까지 ( <span class="blank-highlight">㉠</span> ) 걸려요?<br>A: 5분쯤 걸려요.<br>ㆍ<br>ㆍ<br>ㆍ<br>B: 저기 신호등 앞에서 <span class="blank-highlight">㉡</span> 세워 주세요.<br>A: 네, 알겠습니다. 2,400원입니다.<br>B: 여기 있어요. 감사합니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['몇 시', '언제', '어떻게', '얼마나'],
+    answer: 3
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 어서 오세요. 어디로 갈까요?<br>B: 서울역으로 가 주세요. 서울역까지 ( <span class="blank-highlight">㉠</span> ) 걸려요?<br>A: 5분쯤 걸려요.<br>ㆍ<br>ㆍ<br>ㆍ<br>B: 저기 신호등 앞에서 <span class="blank-highlight">㉡</span> 세워 주세요.<br>A: 네, 알겠습니다. 2,400원입니다.<br>B: 여기 있어요. 감사합니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>과 바꿔 쓸 수 있는 말을 고르세요.',
+    options: ['가 주세요', '타 주세요', '내려 주세요', '기다려 주세요'],
+    answer: 2
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 스커트 좀 보여 주세요.<br>B: 이 스커트 어떠세요? 한번 입어 보세요.<br>A: 네, 주세요.<br>ㆍ<br>ㆍ<br>ㆍ<br>B: 어떠세요? 예쁘지요?<br>A: 네, 예쁘지만 저한테 좀 ( <span class="blank-highlight">㉠</span> ).<br>B: 긴 스커트 안 좋아하세요?<br>A: ( <span class="blank-highlight">㉡</span> ), 긴 스커트를 잘 안 입어요. 좀 짧은 스커트는 없어요?<br>B: ( <span class="blank-highlight">㉢</span> ). 있어요. 잠깐만 기다리세요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['기네요', '높네요', '짧네요', '크네요'],
+    answer: 0
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 스커트 좀 보여 주세요.<br>B: 이 스커트 어떠세요? 한번 입어 보세요.<br>A: 네, 주세요.<br>ㆍ<br>ㆍ<br>ㆍ<br>B: 어떠세요? 예쁘지요?<br>A: 네, 예쁘지만 저한테 좀 ( <span class="blank-highlight">㉠</span> ).<br>B: 긴 스커트 안 좋아하세요?<br>A: ( <span class="blank-highlight">㉡</span> ), 긴 스커트를 잘 안 입어요. 좀 짧은 스커트는 없어요?<br>B: ( <span class="blank-highlight">㉢</span> ). 있어요. 잠깐만 기다리세요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>, <span class="blank-highlight">㉢</span>에 각각 들어갈 말로 알맞게 짝지어진 것을 고르세요.',
+    options: ['네 - 네', '네 - 아니요', '아니요 - 네', '아니요 - 아니요'],
+    answer: 2
+  }
+];
