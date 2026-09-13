42816a2b7d63bcd15c21f0d0d9f93a46a36b8dc3
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-13 23:37:11 +0800

    Add SNU 2B review2 evaluation

---
+++ workbook/snu2b/review2/questions.js
@@ -0,0 +1,168 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 나나, 여기에 <span class="source-underline">차를 세우면 안 돼</span>.<br>B: 아, 저기 (      ) 표지판이 있네. 몰랐어.',
+    options: ['금연', '주차 금지', '사진 촬영 금지', '음식물 반입 금지'],
+    answer: 1
+  },
+  {
+    id: 2,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 감기 다 <span class="source-underline">나았어요?</span><br>B: 네, 집에서 쉬니까 (      ) 것 같아요.',
+    options: ['놓친', '그만둔', '궁금한', '좋아진'],
+    answer: 3
+  },
+  {
+    id: 3,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 시골 인구가 많이 <span class="source-underline">준</span> 것 같아요.<br>B: 네, 일을 찾아서 도시로 가는 사람들이 (      ) 있어요.',
+    options: ['많고', '늘고', '나가고', '오르고'],
+    answer: 1
+  },
+  {
+    id: 4,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 우리가 이 학교에 <span class="source-underline">입학했을</span> 때가 벌써 사 년 전이네.<br>B: 시간 정말 빠르다. 대학 생활이 끝나고 벌써 (      ) 때가 됐네.',
+    options: ['승진할', '유학할', '졸업할', '은퇴할'],
+    answer: 2
+  },
+  {
+    id: 5,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 새로 이사한 집은 어때?\nB: (      )이 잘되톴 있어서 편해.',
+    options: ['방값', '시설', '전망', '생활'],
+    answer: 1
+  },
+  {
+    id: 6,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 이 집이 마음에 드는데 (      )가 얼마예요?\nB: 방값은 한 달에 40만 원이에요.',
+    options: ['월세', '전세', '관리비', '생활비'],
+    answer: 0
+  },
+  {
+    id: 7,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 아직 한국어를 잘 못해서 친구 (      )이 좀 어려워.\nB: 그럼 동호회에 가입하는 게 어때? 취미 생활을 같이하면 빨리 친해질 거야.',
+    options: ['부르는 것', '사귀는 것', '소개하는 것', '도와주는 것'],
+    answer: 1
+  },
+  {
+    id: 8,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 지난주보다 과일값이 많이 (      ).\nB: 네, 요즘 비가 많이 와서 좀 비싸진 것 같네요.',
+    options: ['높았톴요', '지났어요', '생겼어요', '올랐톴요'],
+    answer: 3
+  },
+  {
+    id: 9,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 히엔 씨는 나중에 대학원에 갈 거예요?\nB: 아니요, 대학교 마치고 나면 바로 (      ) 싶톴요.',
+    options: ['결정하고', '이사하고', '취직하고', '양보하고'],
+    answer: 2
+  },
+  {
+    id: 10,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 오늘 철수가 기분이 안 좋은 것 같은데 왜 그래요?\nB: 형하고 싸워서 엄마한테 야단을 (      ).',
+    options: ['시켰톴요', '지었어요', '받았어요', '맞톴요'],
+    answer: 3
+  },
+  {
+    id: 11,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '한국에 오기 전에 회사에 다녔어요.',
+      '이 집은 전망이 좋기는 하지만 좀 비싸요.',
+      '우리 집은 시장이 가까워서 장보기가 편해요.',
+      '한국어 공부를 마치기 후에 고향에 갈 거예요.'
+    ],
+    answer: 3
+  },
+  {
+    id: 12,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '휴일이기 때문에 같이 산에 갑시다.',
+      '다음 달에 고향에 돌아가게 됐톴요.',
+      '작년에 한복을 입어 본 적이 있어요.',
+      '친구들과 이야기할 때 반말을 해도 돼요.'
+    ],
+    answer: 0
+  },
+  {
+    id: 13,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 이 공원에서 음식을 먹어도 되나요?\nB: 네, (      ).',
+    options: ['먹어도 됩니다', '먹기가 좋습니다', '먹톴 버렸습니다', '먹으면 좋겠습니다'],
+    answer: 0
+  },
+  {
+    id: 14,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 이제 정말 봄이 온 것 같네요.\nB: 네, 날씨가 많이 (      ).',
+    options: ['따뜻해졌톴요', '따뜻해 보여요', '따뜻했으면 좋겠어요', '따뜻할지 모르겠톴요'],
+    answer: 0
+  },
+  {
+    id: 15,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 히엔 씨, 켈리 씨와 오래된 친구예요?\nB: 아니요, 이번 학기에 같은 반이 되톴서 (      ).',
+    options: ['알면 돼요', '알게 됐어요', '알고 싶었톴요', '알려고 했어요'],
+    answer: 1
+  },
+  {
+    id: 16,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 지금 살고 있는 집은 톴때요?\nB: 아주 좋아요. 교통이 편리해서 학교 (      ) 편해요.',
+    options: ['다니기', '다니는지', '다녔으면', '다닌 후에'],
+    answer: 0
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 켈리, 내일 뭐 해? 수업 ( <span class="blank-highlight">㉠</span> ) 같이 쇼핑하러 갈래?<br>B: 나도 가고 싶은데 내일은 아르바이트하러 가야 해.<br>A: 아르바이트? 요즘 아르바이트해?<br>B: 응. 일주일에 두 번씩 아이들에게 영톴를 가르치고 있어. 집세가 올라서 요즘 생활비가 좀 많이 ( <span class="blank-highlight">㉡</span> ).<br>A: 그래. 그럼 나중에 시간 날 때 보자.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['끝나다가', '끝나면서', '끝난 후에', '끝나기 때문에'],
+    answer: 2
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 켈리, 내일 뭐 해? 수업 ( <span class="blank-highlight">㉠</span> ) 같이 쇼핑하러 갈래?<br>B: 나도 가고 싶은데 내일은 아르바이트하러 가야 해.<br>A: 아르바이트? 요즘 아르바이트해?<br>B: 응. 일주일에 두 번씩 아이들에게 영톴를 가르치고 있어. 집세가 올라서 요즘 생활비가 좀 많이 ( <span class="blank-highlight">㉡</span> ).<br>A: 그래. 그럼 나중에 시간 날 때 보자.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['있톴', '걸려', '들어', '늘톴'],
+    answer: 2
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '혼자 사는 것은 조금 ( <span class="blank-highlight">㉠</span> ) 아주 좋은 경험입니다. 저는 한국에 와서 집도 혼자 구하고 요리와 청소도 하게 됐습니다. 한국에 오기 전에 이런 일들은 부모님이 해 주셨는데 이제는 제가 다 하고 있습니다. 그리고 아르바이트도 하고 공부도 하면서 열심히 생활하고 있습니다. 저는 전보다 조금 ( <span class="blank-highlight">㉡</span> ) 어른이 된 것 같습니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 들톴갈 알맞은 것을 고르세요.',
+    options: ['힘드니까', '힘들었을 때', '힘들어 보여서', '힘들기는 하지만'],
+    answer: 3
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '혼자 사는 것은 조금 ( <span class="blank-highlight">㉠</span> ) 아주 좋은 경험입니다. 저는 한국에 와서 집도 혼자 구하고 요리와 청소도 하게 됐습니다. 한국에 오기 전에 이런 일들은 부모님이 해 주셨는데 이제는 제가 다 하고 있습니다. 그리고 아르바이트도 하고 공부도 하면서 열심히 생활하고 있습니다. 저는 전보다 조금 ( <span class="blank-highlight">㉡</span> ) 어른이 된 것 같습니다.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['더', '막', '자꾸', '마침'],
+    answer: 0
+  }
+];
