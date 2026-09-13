98d830348187f5c3b8ae598d68b97c69f770a499
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-14 00:03:50 +0800

    Complete SNU 2B review3 evaluation

---
+++ workbook/snu2b/review3/questions.js
@@ -0,0 +1,168 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 추석에 고향에 가려면 표를 빨리 사 <span class="source-underline">놓아야</span> 돼요.<br>B: 네, 저는 일주일 전에 예매해 (      ).',
+    options: ['있어요', '주었어요', '두었어요', '버렸어요'],
+    answer: 2
+  },
+  {
+    id: 2,
+    group: '[1-2] 밑줄 친 것과 의미가 같은 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 지금도 눈이 <span class="source-underline">와요?</span><br>B: 네, 아직 (      ) 있어요.',
+    options: ['피고', '불고', '들고', '내리고'],
+    answer: 3
+  },
+  {
+    id: 3,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 지금 태국은 비가 많이 와서 <span class="source-underline">습도가 높아요</span>.<br>B: 그래요? 한국은 비가 안 와서 (      ).',
+    options: ['쌀쌀해요', '건조해요', '내려가요', '익숙해요'],
+    answer: 1
+  },
+  {
+    id: 4,
+    group: '[3-4] 밑줄 친 것과 의미가 반대되는 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 아침에 휴대 전화 <span class="source-underline">고치러</span> 갔다 왔어요.<br>B: 어디가 (      )?',
+    options: ['넘어졌어요', '떨어졌어요', '사고 났어요', '고장 났어요'],
+    answer: 3
+  },
+  {
+    id: 5,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 스티븐 씨, 지금 뭐 하고 있어요?\nB: 방이 더러워서 청소기 (      ) 있어요.',
+    options: ['닦고', '쓰고', '돌리고', '만지고'],
+    answer: 2
+  },
+  {
+    id: 6,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 벌써 음식 만들 재료를 다 샀어요?\nB: 네, 내일은 시장이 문을 닫으니까 오늘 (      ) 샀어요.',
+    options: ['미리', '자꾸', '계속', '새로'],
+    answer: 0
+  },
+  {
+    id: 7,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 지금 천 원짜리가 없는데 어떻게 하지요?\nB: 걱정하지 마세요. 제가 (      ) 낼게요.',
+    options: ['내내', '대신', '원래', '갑자기'],
+    answer: 1
+  },
+  {
+    id: 8,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 나나 씨, 건강하게 잘 지내세요. 많이 보고 싶을 거예요.\nB: 네, 저도 히엔 씨가 정말 (      ) 거예요.',
+    options: ['새로울', '그리울', '익숙할', '소중할'],
+    answer: 1
+  },
+  {
+    id: 9,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 샤오밍 씨, 다리가 왜 그래요? 다쳤어요?\nB: 네, 농구하다가 다른 사람하고 (      ) 넘어졌어요.',
+    options: ['붙어서', '부딪혀서', '떨어뜨려서', '미끄러워서'],
+    answer: 1
+  },
+  {
+    id: 10,
+    group: '[5-10] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 유진 씨는 한국 생활 중에 어떤 일이 가장 (      )?\nB: 친구들하고 같이 여행 갔을 때가 정말 즐거웠어요.',
+    options: ['후회가 돼요', '짜증이 나요', '사랑에 빠져요', '기억에 남아요'],
+    answer: 3
+  },
+  {
+    id: 11,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '작년에 산 옷이 잘 맞지 않는다.',
+      '집 앞에 있는 가게에서 신문을 판다.',
+      '나나 씨가 신은 신발이 참 편해 보인다.',
+      '저는 수업이 끝나면 바로 집에 가고 싶다.'
+    ],
+    answer: 3
+  },
+  {
+    id: 12,
+    group: '[11-12] 틀린 문장을 고르세요.',
+    prompt: '',
+    options: [
+      '하늘이 파랗니까 기분이 참 좋아요.',
+      '노란 단풍잎이 많이 떨어져 있어요.',
+      '아이의 눈이 까매서 참 예뻐 보여요.',
+      '어젯밤에 온 눈 때문에 밖이 하얘졌어요.'
+    ],
+    answer: 0
+  },
+  {
+    id: 13,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 아침에 지하철에서 잠이 들어서 못 (      ).\nB: 저도 요즘 가끔 그럴 때가 있어요.',
+    options: ['내릴 뻔했어요', '내린 것 같아요', '내리려고 했어요', '내릴지 모르겠어요'],
+    answer: 0
+  },
+  {
+    id: 14,
+    group: '[13-14] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 방학 계획 세웠어요?\nB: 아직 결정하지는 못했는데 기타를 (      ).',
+    options: ['배워 볼까 해요', '배우게 되었어요', '배우기로 했어요', '배우는 편이에요'],
+    answer: 0
+  },
+  {
+    id: 15,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 공항에 갔다 왔어요?\nB: 네, 친구가 오늘 고향에 돌아가서 공항까지 (      ) 왔어요.',
+    options: ['사다 주고', '갔다 주려고', '데려다 주고', '모셔다 드리러'],
+    answer: 2
+  },
+  {
+    id: 16,
+    group: '[15-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 김 선생님께서 지금 댁에 계실까요?\nB: 아니요, 수업 때문에 학교에 (      ) 학교로 전화해 보세요.',
+    options: ['계실 거라서', '계실 테니까', '계시기 때문에', '계실 것 같아서'],
+    answer: 1
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 어떻게 오셨습니까?<br>B: 조금 전에 여기에 휴대 전화를 놓고 나갔는데요.<br>A: 휴대 전화가 어떻게 ( <span class="blank-highlight">㉠</span> )?<br>B: 하얀색이고 까만색 인형이 ( <span class="blank-highlight">㉡</span> ) 있어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['생겼어요', '들었어요', '놓았어요', '그랬어요'],
+    answer: 0
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 어떻게 오셨습니까?<br>B: 조금 전에 여기에 휴대 전화를 놓고 나갔는데요.<br>A: 휴대 전화가 어떻게 ( <span class="blank-highlight">㉠</span> )?<br>B: 하얀색이고 까만색 인형이 ( <span class="blank-highlight">㉡</span> ) 있어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['누워', '열려', '달려', '놓여'],
+    answer: 2
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '( <span class="blank-highlight">㉠</span> ) 12월이 되었다. 이제 올해도 며칠밖에 남지 않았다. 지나간 시간들을 생각해 보면 힘든 일이 많이 있었다. 하지만 힘들 때 함께해 준 친구들이 있어서 그 시간을 잘 보낼 수 있었다. 친구들에게 받은 소중한 사랑을 나도 다른 사람들에게 꼭 ( <span class="blank-highlight">㉡</span> ).',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['벌써', '내내', '거의', '혹시'],
+    answer: 0
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: '( <span class="blank-highlight">㉠</span> ) 12월이 되었다. 이제 올해도 며칠밖에 남지 않았다. 지나간 시간들을 생각해 보면 힘든 일이 많이 있었다. 하지만 힘들 때 함께해 준 친구들이 있어서 그 시간을 잘 보낼 수 있었다. 친구들에게 받은 소중한 사랑을 나도 다른 사람들에게 꼭 ( <span class="blank-highlight">㉡</span> ).',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 들어갈 알맞은 것을 고르세요.',
+    options: ['돌려줄 뻔했다', '돌려주고 싶다', '돌려줘도 된다', '돌려준 적이 있다'],
+    answer: 1
+  }
+];
