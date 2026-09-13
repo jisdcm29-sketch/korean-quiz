e06ab416393844711c00458ad05ec4aee63b6119
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-13 14:09:21 +0800

    Add files via upload

---
+++ workbook/snu1b/review2/questions.js
@@ -0,0 +1,166 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-5] 그림을 보고 ( )에 알맞은 것을 고르세요.',
+    image: 'assets/q01.png',
+    prompt: 'A: ( &nbsp;&nbsp;&nbsp;&nbsp; )이/가 어떻게 되세요?<br>B: 880-5488이에요.',
+    promptHtml: true,
+    options: ['가족', '연세', '직업', '전화번호'],
+    answer: 3
+  },
+  {
+    id: 2,
+    group: '[1-5] 그림을 보고 ( )에 알맞은 것을 고르세요.',
+    image: 'assets/q02.png',
+    prompt: 'A: ( &nbsp;&nbsp;&nbsp;&nbsp; ).<br>B: 네, 한국어센터 사무실입니다.',
+    promptHtml: true,
+    options: ['미안해요', '여보세요', '어서 오세요', '오랜만이에요'],
+    answer: 1
+  },
+  {
+    id: 3,
+    group: '[1-5] 그림을 보고 ( )에 알맞은 것을 고르세요.',
+    image: 'assets/q03.png',
+    prompt: 'A: 지금 뭐 해요?<br>B: 친구한테 ( &nbsp;&nbsp;&nbsp;&nbsp; )을/를 보내고 있어요.',
+    promptHtml: true,
+    options: ['문자', '선물', '엽서', '편지'],
+    answer: 0
+  },
+  {
+    id: 4,
+    group: '[1-5] 그림을 보고 ( )에 알맞은 것을 고르세요.',
+    image: 'assets/q04.png',
+    prompt: 'A: 어떻게 오셨어요?<br>B: ( &nbsp;&nbsp;&nbsp;&nbsp; )이/가 아파서 왔어요.',
+    promptHtml: true,
+    options: ['가슴', '다리', '어깨', '허리'],
+    answer: 3
+  },
+  {
+    id: 5,
+    group: '[1-5] 그림을 보고 ( )에 알맞은 것을 고르세요.',
+    image: 'assets/q05.png',
+    prompt: 'A: 감기는 좀 어때요?<br>B: 괜찮아요. 지금은 ( &nbsp;&nbsp;&nbsp;&nbsp; )만 나요.',
+    promptHtml: true,
+    options: ['열', '기침', '눈물', '콧물'],
+    answer: 3
+  },
+  {
+    id: 6,
+    groupHtml: true,
+    group: '[6-7] 밑줄 친 부분과 <span class="blank-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    prompt: 'A: 아키라 씨한테 전화를 받았어요?\nB: 아니요, 제가 전화를 (      ).',
+    options: ['샀어요', '했어요', '보냈어요', '기다렸어요'],
+    answer: 1
+  },
+  {
+    id: 7,
+    groupHtml: true,
+    group: '[6-7] 밑줄 친 부분과 <span class="blank-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    prompt: 'A: 내일 시간 있어요?\nB: 미안하지만 내일은 (      ).',
+    options: ['고파요', '나빠요', '바빠요', '아파요'],
+    answer: 2
+  },
+  {
+    id: 8,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '(      )이/가 아프세요?',
+    options: ['누구', '며칠', '무엇', '어디'],
+    answer: 3
+  },
+  {
+    id: 9,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '전화번호가 (      )번이에요?',
+    options: ['몇', '무슨', '어느', '얼마'],
+    answer: 0
+  },
+  {
+    id: 10,
+    group: '[8-10] (      )에 알맞은 것을 고르세요.',
+    prompt: '수업 시간에는 휴대폰을 (      ) 돼요.',
+    options: ['꺼야', '사야', '받아야', '찾아야'],
+    answer: 0
+  },
+  {
+    id: 11,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 저 영화 재미있어요?\nB: 아니요, 재미없어요. (      ).',
+    options: ['못 봤어요', '볼 거예요', '봐야 돼요', '보지 마세요'],
+    answer: 3
+  },
+  {
+    id: 12,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 어떻게 오셨어요?\nB: 기침을 하고 목이 많이 (      ).',
+    options: ['아파요', '아팠어요', '아프세요', '안 아파요'],
+    answer: 0
+  },
+  {
+    id: 13,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 선생님, 점심 드셨지요?\nB: 네, 조금 전에 (      ).',
+    options: ['드셨어요', '먹었어요', '먹었지요', '안 먹었어요'],
+    answer: 1
+  },
+  {
+    id: 14,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 이번 방학에 고향에 가요?\nB: 아니요, 일이 많아서 (      ).',
+    options: ['가지요', '못 가요', '가야 돼요', '가지 마세요'],
+    answer: 1
+  },
+  {
+    id: 15,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 시험 잘 봤어요?\nB: 아니요, 너무 (      ) 잘 못 봤어요.',
+    options: ['어렵고', '어렵지만', '어려워서', '어려웠지만'],
+    answer: 2
+  },
+  {
+    id: 16,
+    group: '[11-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 선생님은 지금 뭐 하세요?\nB: 지금 사무실에서 (      ).',
+    options: ['전화하셨어요', '전화하실 거예요', '전화하고 계세요', '전화하고 있었어요'],
+    answer: 2
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 여보세요, 샤오밍 씨 휴대폰이지요?\nB: 네, 나나 씨, 저예요.\nA: 샤오밍 씨, 왜 오늘 학교에 안 왔어요?\nB: 감기에 ( <span class="blank-highlight">㉠</span> ) 못 갔어요.\nA: 그래요? 병원에는 갔어요?\nB: 아니요, 약 ( <span class="blank-highlight">㉡</span> ) 먹었어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['걸려서', '걸리고', '걸렸고', '걸리지만'],
+    answer: 0
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 여보세요, 샤오밍 씨 휴대폰이지요?\nB: 네, 나나 씨, 저예요.\nA: 샤오밍 씨, 왜 오늘 학교에 안 왔어요?\nB: 감기에 ( <span class="blank-highlight">㉠</span> ) 못 갔어요.\nA: 그래요? 병원에는 갔어요?\nB: 아니요, 약 ( <span class="blank-highlight">㉡</span> ) 먹었어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞은 것을 고르세요.',
+    options: ['도', '만', '이', '까지'],
+    answer: 1
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 유진 씨도 쇼핑 좋아하지요? 오후에 같이 쇼핑 갈까요?\nB: 미안하지만 내일 시험이 있어서 오늘은 ( <span class="blank-highlight">㉠</span> ).\nA: 아, 그래요? 그럼 이번 주말에는 어때요?\nB: 주말에는 리포트를 ( <span class="blank-highlight">㉡</span> ) 바빠요. 미안해요.\nA: 많이 바쁘네요. 그럼 다음에 같이 가요.\nB: 네, 그래요. 고마워요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['공부 안 해요', '공부해야 돼요', '공부하고 있어요', '공부하지 마세요'],
+    answer: 1
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 유진 씨도 쇼핑 좋아하지요? 오후에 같이 쇼핑 갈까요?\nB: 미안하지만 내일 시험이 있어서 오늘은 ( <span class="blank-highlight">㉠</span> ).\nA: 아, 그래요? 그럼 이번 주말에는 어때요?\nB: 주말에는 리포트를 ( <span class="blank-highlight">㉡</span> ) 바빠요. 미안해요.\nA: 많이 바쁘네요. 그럼 다음에 같이 가요.\nB: 네, 그래요. 고마워요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞은 것을 고르세요.',
+    options: ['써서', '쓰고', '써야 돼서', '쓸 거지만'],
+    answer: 2
+  }
+];
