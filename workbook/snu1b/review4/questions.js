4ad8c2be3b3e82fbd3001c26f9f526f9dc45f79c
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-13 16:10:35 +0800

    Add files via upload

---
+++ workbook/snu1b/review4/questions.js
@@ -0,0 +1,179 @@
+window.WORKBOOK_REVIEW_QUESTIONS = [
+  {
+    id: 1,
+    group: '[1-4] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q01.png',
+    prompt: 'A: 히엔 씨, 생일 (      ).\nB: 고마워요.',
+    options: ['식사해요', '연락해요', '준비해요', '축하해요'],
+    answer: 3
+  },
+  {
+    id: 2,
+    group: '[1-4] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q02.png',
+    prompt: 'A: 오늘 새벽 5시에 일어났어요.\nB: 왜 (      ) 일어났어요?',
+    options: ['늦게', '많이', '일찍', '조금'],
+    answer: 2
+  },
+  {
+    id: 3,
+    group: '[1-4] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q03.png',
+    prompt: 'A: 어디에 가요?\nB: (      ) 사러 가게에 가요.',
+    options: ['약', '빵', '음식', '음료수'],
+    answer: 0
+  },
+  {
+    id: 4,
+    group: '[1-4] 그림을 보고 (      )에 알맞은 것을 고르세요.',
+    image: 'assets/q04.png',
+    prompt: 'A: 내일 우리집에 올 수 있어요?\nB: 아니요, 내일은 (      ).',
+    options: ['춤춰야 돼요', '이사해야 돼요', '청소해야 돼요', '빨래해야 돼요'],
+    answer: 1
+  },
+  {
+    id: 5,
+    groupHtml: true,
+    group: '[5-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: <span class="opposite-highlight">빨리</span> 갈게요. 잠깐만 기다리세요.<br>B: (      ) 오세요. 안 늦었어요.',
+    options: ['잘', '많이', '열심히', '천천히'],
+    answer: 3
+  },
+  {
+    id: 6,
+    groupHtml: true,
+    group: '[5-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 아침 <span class="opposite-highlight">많이</span> 먹었어요?<br>B: 아니요. 배가 안 고파서 (      ) 먹었어요.',
+    options: ['참', '푹', '빨리', '조금'],
+    answer: 3
+  },
+  {
+    id: 7,
+    groupHtml: true,
+    group: '[5-7] 밑줄 친 부분과 <span class="opposite-highlight">반대되는 뜻</span>을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 기타를 <span class="opposite-highlight">잘</span> 쳐요?<br>B: 아니요, (      ) 쳐요.',
+    options: ['꼭', '못', '안', '쭉'],
+    answer: 1
+  },
+  {
+    id: 8,
+    group: '[8-10] 밑줄 친 부분과 같은 뜻을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 경치가 정말 <u class="source-underline">예쁘네요</u>.<br>B: 네, 참 (      ).',
+    options: ['귀여워요', '따뜻해요', '즐거워요', '아름다워요'],
+    answer: 3
+  },
+  {
+    id: 9,
+    group: '[8-10] 밑줄 친 부분과 같은 뜻을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 학생 식당에서 점심 <u class="source-underline">먹었어요?</u><br>B: 네, 학교에서 (      ).',
+    options: ['드셨어요', '마셨어요', '식사했어요', '준비했어요'],
+    answer: 2
+  },
+  {
+    id: 10,
+    group: '[8-10] 밑줄 친 부분과 같은 뜻을 가진 것을 고르세요.',
+    promptHtml: true,
+    prompt: 'A: 몇 시에 <u class="source-underline">출발했어요?</u><br>B: 서울역에서 11시에 (      ) 기차를 탔어요.',
+    options: ['떠나는', '도착하는', '돌아오는', '예매하는'],
+    answer: 0
+  },
+  {
+    id: 11,
+    group: '[11-12] 밑줄 친 부분이 틀린 것을 고르세요.',
+    optionHtml: true,
+    prompt: '',
+    options: [
+      '아침에 <u class="source-underline">샤워하면서</u> 노래합니다.',
+      '유진 씨는 팝콘을 <u class="source-underline">먹으면서</u> 영화를 봅니다.',
+      '어머니가 책을 읽어 <u class="source-underline">주시면서</u> 아기가 잡니다.',
+      '아키라 씨는 한국어를 <u class="source-underline">공부하면서</u> 회사에 다닙니다.'
+    ],
+    answer: 2
+  },
+  {
+    id: 12,
+    group: '[11-12] 밑줄 친 부분이 틀린 것을 고르세요.',
+    optionHtml: true,
+    prompt: '',
+    options: [
+      '<u class="source-underline">사장님이 좋아하시는 음식은</u> 불고기입니다.',
+      '<u class="source-underline">켈리 씨가 마시는 커피는</u> 에스프레소입니다.',
+      '<u class="source-underline">아키라 씨는 다니는 회사는</u> 월드여행사입니다.',
+      '<u class="source-underline">한국 사람들이 자주 먹는</u> 음식은 김치입니다.'
+    ],
+    answer: 2
+  },
+  {
+    id: 13,
+    group: '[13-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 고향에 돌아가면 편지 쓰세요.\nB: 네, (      ).',
+    options: ['썼어요', '쓸게요', '써 주세요', '쓸 수 있어요'],
+    answer: 1
+  },
+  {
+    id: 14,
+    group: '[13-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 내일 아키라 씨를 만나요?\nB: 아키라 씨 일이 일찍 (      ) 만날 거예요.',
+    options: ['끝나는', '끝나려', '끝나면', '끝나면서'],
+    answer: 2
+  },
+  {
+    id: 15,
+    group: '[13-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 왜 한국에 왔어요?\nB: (      ) 왔어요.',
+    options: ['여행하고', '여행하러', '여행하면', '여행해서'],
+    answer: 1
+  },
+  {
+    id: 16,
+    group: '[13-16] (      )에 알맞은 것을 고르세요.',
+    prompt: 'A: 마리코 씨가 이 김치를 만들었어요?\nB: 네, 제가 요리책을 (      ) 만들었어요.',
+    options: ['보러', '봐서', '보면서', '봤지만'],
+    answer: 2
+  },
+  {
+    id: 17,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 뭘 먹을까요?<br>B: 날씨가 추워서 ( <span class="blank-highlight">㉠</span> ) 음식을 먹고 싶어요.<br>A: 그럼 갈비탕 먹으러 가요.<br>B: 아, 갈비탕을 먹어 봤어요?<br>A: 네, 룸메이트가 ( <span class="blank-highlight">㉡</span> ) 자주 먹었어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 알맞은 것을 고르세요.',
+    options: ['매운', '추운', '따뜻한', '똑똑한'],
+    answer: 2
+  },
+  {
+    id: 18,
+    group: '[17-18] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 뭘 먹을까요?<br>B: 날씨가 추워서 ( <span class="blank-highlight">㉠</span> ) 음식을 먹고 싶어요.<br>A: 그럼 갈비탕 먹으러 가요.<br>B: 아, 갈비탕을 먹어 봤어요?<br>A: 네, 룸메이트가 ( <span class="blank-highlight">㉡</span> ) 자주 먹었어요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞은 것을 고르세요.',
+    options: ['먹으면', '먹으면서', '먹고 싶어서', '먹고 싶어 해서'],
+    answer: 3
+  },
+  {
+    id: 19,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 여보세요. 아키라 씨? 지금 어디예요? 집이에요?<br>B: 아니요, 차 안이에요.<br>A: 아. ( <span class="blank-highlight">㉠</span> ) 전화할 수 있어요?<br>B: 괜찮아요. 아직 출발 안 했어요.<br>무슨 일이 있어요?<br>A: 내일 시험이 끝나면 파티를 하려고 해요. 올 수 있어요?<br>B: 그럼요. ( <span class="blank-highlight">㉡</span> ) 고마워요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉠</span>에 들어갈 말로 알맞은 것을 고르세요.',
+    options: ['운전해서', '운전하면서', '운전했지만', '운전할 수 있으면'],
+    answer: 1
+  },
+  {
+    id: 20,
+    group: '[19-20] 다음을 읽고 질문에 답하세요.',
+    contextHtml: true,
+    context: 'A: 여보세요. 아키라 씨? 지금 어디예요? 집이에요?<br>B: 아니요, 차 안이에요.<br>A: 아. ( <span class="blank-highlight">㉠</span> ) 전화할 수 있어요?<br>B: 괜찮아요. 아직 출발 안 했어요.<br>무슨 일이 있어요?<br>A: 내일 시험이 끝나면 파티를 하려고 해요. 올 수 있어요?<br>B: 그럼요. ( <span class="blank-highlight">㉡</span> ) 고마워요.',
+    promptHtml: true,
+    prompt: '<span class="blank-highlight">㉡</span>에 알맞은 것을 고르세요.',
+    options: ['계획해 줘서', '선물해 줘서', '초대해 줘서', '축하해 줘서'],
+    answer: 2
+  }
+];
